ddc2bb8f10b4ce152d7ba455b27b9ce4303a2fd9
KairuiLiu <tclkr@live.com>
2022-09-07 18:38:18 +0800

    feat: three init

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,11 +1,19 @@
-import Core from '../core';
-import ui from '../ui';
+import * as THREE from 'three';
 
 class Core {
 	canvas: HTMLCanvasElement;
 
+	camera: THREE.PerspectiveCamera;
+
+	scene: THREE.Scene;
+
+	renderer: THREE.WebGLRenderer;
+
 	constructor(canvas: HTMLCanvasElement) {
 		this.canvas = canvas;
+		this.camera = new THREE.PerspectiveCamera();
+		this.scene = new THREE.Scene();
+		this.renderer = new THREE.WebGLRenderer();
 	}
 }
 
