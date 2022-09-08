c3cdc603d245bb1d9ed7808b91e5d031ce237c6b
KairuiLiu <tclkr@live.com>
2022-09-08 14:55:39 +0800

    fix: egg

--- src/utils/useless.ts
+++ src/utils/useless.ts
@@ -7,6 +7,15 @@
 	const noTrackConsole = function (texts: string[], cmd = 'log') {
 		setTimeout(() => console[cmd](...texts), 0);
 	};
+	const e1 = ['e', 'a', 's', 't', 'e', 'r'].reduce((pre, cur) =>
+		Math.random() > 1 ? '' : pre + cur
+	);
+	const e2 = ['e', 'g', 'g'].reduce((pre, cur) =>
+		Math.random() > 1 ? '' : pre + cur
+	);
+	const ee = ['E', 'a', 's', 't', 'e', 'r', 'E', 'g', 'g'].reduce((pre, cur) =>
+		Math.random() > 1 ? '' : pre + cur
+	);
 
 	if (lang === 'cn') {
 		// EGG 00
@@ -25,37 +34,34 @@
 	} else {
 		// EGG 00
 		noTrackConsole(['%c🥚', 'font-size: 96px'], 'log');
-		noTrackConsole([`🎊 Wow, you found a hidden easter egg (#0)`], 'log');
+		noTrackConsole([`🎊 Wow, you found a hidden ${e1} ${e2} (#0)`], 'log');
 		noTrackConsole([], 'clear');
 		noTrackConsole(
 			[
-				`It's ${yearInv}, who still hides easter eggs in the console? ┑(￣Д ￣)┍`,
+				`It's ${yearInv}, who still hides ${e1} ${e2} in the console? ┑(￣Д ￣)┍`,
 			],
 			'groupCollapsed'
 		);
 
 		// EGG 01
 		noTrackConsole(['%c🥚', 'font-size: 96px;'], 'log');
-		noTrackConsole([`Hee, you found an unremarkable easter egg (#1)`], 'log');
+		noTrackConsole([`Hee, you found an unremarkable ${e1} ${e2} (#1)`], 'log');
 		noTrackConsole([], 'groupEnd');
 	}
 
 	// EGG 02
-	fetch(`${window.location.href}/easter_egg`).catch(Function.prototype);
+	fetch(`${window.location.host}/${e1}_${e2}`).catch(Function.prototype);
 
 	// EGG 03
 
-	const className = ['E', 'a', 's', 't', 'e', 'r', 'E', 'g', 'g'].reduce(
-		(pre, cur) => (Math.random() > 1 ? '' : pre + cur)
-	);
 	let t = null;
 	eval(`
-		const ${className} = function () {
+		const ${ee} = function () {
 			this.cn = '🥚 真不错, 埋的这么深都能被发现 (#3)';
 			this.en = '🥚 Nice, hiding so deep still be found (#3)';
 			this.keepAlive = Math.random();
 		};
-		t = new ${className}()
+		t = new ${ee}()
 	`);
 	setTimeout(() => {
 		if (t.keepAlive > 1) {
@@ -63,7 +69,7 @@
 				noTrackConsole(['🥚 理论上这是一颗永远都不会触发的彩蛋 (#-1)'], 'log');
 			else
 				noTrackConsole(
-					['🥚 In theory this is an Easter egg that will never trigger (#-1)'],
+					[`🥚 In theory this is an ${e1} ${e2} that will never trigger (#-1)`],
 					'log'
 				);
 		}
@@ -72,8 +78,8 @@
 
 	// EGG 04
 	setTimeout(() => {
-		sessionStorage.setItem('easter_egg_cn', '🥚 一颗稀松平常的彩蛋 (#4)');
-		sessionStorage.setItem('easter_egg_en', '🥚 an ordinary egg (#4)');
+		sessionStorage.setItem(`${e1}_${e2}_cn`, '🥚 一颗稀松平常的彩蛋 (#4)');
+		sessionStorage.setItem(`${e1}_${e2}_en`, `🥚 an ordinary ${e2} (#4)`);
 	});
 
 	// EGG 05
